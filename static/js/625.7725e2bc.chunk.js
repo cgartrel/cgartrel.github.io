b048dcdbf621662727440cce5f2aa1ef4db4f1e9
Mike Gartrell <cgartrel@gmail.com>
2025-05-03 18:08:22 +0200

    Updates reflecting my work at Sigma Nova.

---
+++ static/js/625.7725e2bc.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[625],{625:(e,s,a)=>{e.exports=a.p+"static/media/about.8f56363d6aea291ad254.md"}}]);
--- static/js/625.f09ec8a9.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[625],{625:(e,s,t)=>{e.exports=t.p+"static/media/about.c15d4c3d1df5e214c9de.md"}}]);
